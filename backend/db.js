1cf16d5def35c9d6200c9848eafd51bc40f22f76
Vishnu <softwarevishnu@gmail.com>
2024-03-14 14:15:51 +0530

    password

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg');
 
 const pool = new Pool({
     user : "postgres",
-    password : "2004",
+    password : "2003",
     host : "localhost",
     port : 5432,
     database : 'bug_tracking'
