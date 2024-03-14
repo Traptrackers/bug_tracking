998cb8bdbf4d206fa4569b464764baa4a82b1e0d
Traptrackers <flipkartdpbh@gmail.com>
2024-03-14 14:10:32 +0530

    test

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg');
 
 const pool = new Pool({
     user : "postgres",
-    password : "2003",
+    password : "2004",
     host : "localhost",
     port : 5432,
     database : 'bug_tracking'
